1e7b12f6e41bedd28af3cd8e6bf61f1a40b49e21
choo8 <choo8@illinois.edu>
2023-11-25 01:03:20 -0800

    Completed Exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+initial_height = 100
+num_bounces, cur_height = 0, initial_height
+
+while num_bounces < 10:
+    num_bounces += 1
+    cur_height *= 0.6
+
+    print(f"{num_bounces} {round(cur_height, 4)}")
