6f7279f91d3d18221c4b873be3ac53ddc1e51244
choo8 <choo8@illinois.edu>
2023-11-26 02:24:08 -0800

    Completed Exercise 2.5

--- Work/report.py
+++ Work/report.py
@@ -11,7 +11,7 @@ def read_portfolio(filename):
         headers = next(rows)
 
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
             portfolio.append(holding)
 
     return portfolio
