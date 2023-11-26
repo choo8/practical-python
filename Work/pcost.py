0edff4b5622aba8a3e72e40150b7f538c8ee0995
choo8 <choo8@illinois.edu>
2023-11-25 21:38:13 -0800

    Completed Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+
 def portfolio_cost(filename):
     total_cost = 0
 
     with open(filename, "rt") as f:
-        lines = f.readlines()
-        for line in lines[1:]:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             try:
-                _, num_shares, price = line.strip().split(",")
+                _, num_shares, price = row
                 total_cost += int(num_shares) * float(price)
             except ValueError as e:
                 print(e)
