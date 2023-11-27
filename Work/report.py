299a4c4dc6550b67fc40835d67306ea93c77400a
choo8 <choo8@illinois.edu>
2023-11-27 00:35:45 -0800

    Completed Exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -2,6 +2,7 @@
 #
 # Exercise 2.4
 import csv
+import sys
 
 def read_portfolio(filename):
     portfolio = []
@@ -15,3 +16,33 @@ def read_portfolio(filename):
             portfolio.append(holding)
 
     return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+
+        for row in rows:
+            if row != []:
+                prices[row[0]] = float(row[1])
+
+    return prices
+
+if len(sys.argv) == 3:
+    portfolio_filename, prices_filename = sys.argv[1], sys.argv[2]
+else:
+    portfolio_filename, prices_filename = "Data/portfolio.csv", "Data/prices.csv"
+
+portfolio = read_portfolio(portfolio_filename)
+prices = read_prices(prices_filename)
+
+cost = 0
+market_value = 0
+
+for share in portfolio:
+    cost += share["shares"] * share["price"]
+    market_value += share["shares"] * prices[share["name"]]
+
+print(f"Current value {market_value:.2f}")
+print(f"Gain {market_value - cost:.2f}")
